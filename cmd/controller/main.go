29c1c257adfb90014c58994fa501a4f22b4ef963
Mowgli <MagikMowgli@users.noreply.github.com>
2025-11-17 17:31:12 +0000

    first k8s go programm - successfully lists pods

---
+++ cmd/controller/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+
+func main() {
+	kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Found %d pods:\n", len(pods.Items))
+	for _, pod := range pods.Items {
+		fmt.Printf("- %s (namespace: %s)\n", pod.Name, pod.Namespace)
+	}
+
+	
+}	
